9371f6b4d0d34086945c7cf36ea58b30d22be147
er-em <fikriramadhan002@gmail.com>
2023-12-15 10:46:01 +0700

    add responsive Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
-import { RouterProvider } from 'react-router-dom';
+import { Link, RouterProvider } from 'react-router-dom';
 import './App.css'
 import { conditionalRouter } from './router';
+import { Box, Image } from '@chakra-ui/react';
 
 function App() {
-  
+
 
   return (
-    <>
+    <Box className="App">
       <RouterProvider router={conditionalRouter} />
-    </>
+    </Box>
+
   )
 }
 
